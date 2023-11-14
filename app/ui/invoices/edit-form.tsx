d9262944ffa847ee4c05c760d9c7162de41cb72f
Sebastian Scherbes <github@scherbes.de>
2023-11-14 08:40:28 +0700

    improving-accessibility

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -4,13 +4,17 @@ import { CustomerField, InvoiceForm } from "@/app/lib/definitions";
 import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { Button } from "@/app/ui/button";
+
 import { updateInvoice } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
 
 export default function EditInvoiceForm({ invoice, customers }: { invoice: InvoiceForm; customers: CustomerField[] }) {
+  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -23,6 +27,7 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue={invoice.customer_id}
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -35,6 +40,13 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          {state.errors?.customerId ? (
+            <div id="customer-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.customerId.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Amount */}
@@ -50,15 +62,24 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
                 type="number"
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
+                aria-describedby="amount-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
+          {state.errors?.amount ? (
+            <div id="amount-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.amount.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </div>
 
         {/* Invoice Status */}
-        <fieldset>
+
+        <fieldset aria-describedby="status-error">
           <legend className="mb-2 block text-sm font-medium">Set the invoice status</legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
@@ -96,8 +117,20 @@ export default function EditInvoiceForm({ invoice, customers }: { invoice: Invoi
               </div>
             </div>
           </div>
+          {state.errors?.status ? (
+            <div id="status-error" aria-live="polite" className="mt-2 text-sm text-red-500">
+              {state.errors.status.map((error: string) => (
+                <p key={error}>{error}</p>
+              ))}
+            </div>
+          ) : null}
         </fieldset>
       </div>
+      {state.message ? (
+        <div className="mt-2 text-sm text-red-500">
+          <p>{state.message}</p>
+        </div>
+      ) : null}
       <div className="mt-6 flex justify-end gap-4">
         <Link
           href="/dashboard/invoices"
